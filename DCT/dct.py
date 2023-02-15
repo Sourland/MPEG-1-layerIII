dd872a6e01744be202cf9ce00387b5ad4510f5db
Dimitrios Sourlantzis <jem2010@windowslive.com>
2023-02-16 00:22:45 +0200

    add tonal masking demo

--- DCT/dct.py
+++ DCT/dct.py
@@ -1,17 +1,22 @@
-from scipy.fft import dct
+from scipy.fft import dct, idct
 import numpy as np
 
 
-def frameDCT(Y):
-    return dct(Y, norm='ortho')
+def frameDCT(Y: np.ndarray):
+    Y_dct = np.zeros(Y.shape)
+    for i in range(Y.shape[0]):
+        Y_dct[i] = dct(Y[i])
+
+    return Y_dct
 
 
-def iframeDCT(Y):
-    return idct(Y, norm='ortho')
+def iframeDCT(Y_dct):
+    Y = np.zeros(Y_dct.shape)
+    for i in range(Y_dct.shape[0]):
+        Y[i] = idct(Y_dct[i])
+    return Y
 
 
 def DCT_power(dct_coefficients):
     norm = np.power(np.abs(dct_coefficients), 2)
     return 10 * np.log10(norm)
-
-
--- audible_threshold/tonal_components.py
+++ audible_threshold/tonal_components.py
@@ -1,5 +1,8 @@
 import numpy as np
-from DCT.dct import DCT_power
+from DCT.dct import DCT_power, frameDCT, iframeDCT
+from scipy.io import wavfile
+from subband_filtering.frame import frame_sub_analysis
+from subband_filtering.mp3 import make_mp3_analysisfb
 
 
 def Dk_Sparse(K_max):
@@ -21,34 +24,58 @@ def ST_init(c: np.ndarray, D):
         neighbors_left = k - np.where(D[k, :] == 1)[0]
         neighbors = np.array([neighbors_right, neighbors_left])
 
+        neighbors = np.delete(neighbors, np.where(neighbors < 0))
+        neighbors = np.delete(neighbors, np.where(neighbors >= m * n))
+
         if P[k] > P[k + 1] and P[k] > P[k - 1]:
             tonal_components.append(k)
+        if neighbors.size == 0:
+            continue
+
         elif np.all(P[k] > (P[neighbors] + 7)):
             tonal_components.append(k)
 
-    return tonal_components
+    return np.unique(tonal_components)
 
 
 def mask_power(c, ST):
     P = DCT_power(c)
+    P = np.ndarray.flatten(P)
     P_M = []
     for masker_idx in ST:
         exponential = 0
         for j in range(-1, 2):
-            exponential += np.power(10, 0.1 * P(masker_idx + j))
-        P_M.append(10*np.log10(exponential))
+            exponential += np.power(10, 0.1 * P[masker_idx + j])
+        P_M.append(10 * np.log10(exponential))
 
     return P_M
 
 
 def ST_reduction(ST, c, Tq):
     P = DCT_power(c)
+    P = np.ndarray.flatten(P)
     for k in ST:
         if P[k] < Tq[k]:
-            ST.delete(k)
+            ST = np.delete(ST, np.where(ST == k))
 
     return ST, P[ST]
 
 
 def spread_function(ST, PM, Kmax):
     ...
+
+
+h = np.load("../h.npy", allow_pickle=True).tolist()["h"]
+M, N = 32, 36
+L = 512
+samplerate, wavin = wavfile.read("../myfile.wav")
+frame = wavin[0:M * N + L]
+H = make_mp3_analysisfb(h, M)
+Yc = frame_sub_analysis(frame, H, N)
+dct = frameDCT(Yc)
+Dk = Dk_Sparse(M * N - 1)
+ST = ST_init(Yc, Dk)
+PM = mask_power(Yc, ST)
+Tq = np.load("../Tq.npy", allow_pickle=True).tolist()[0]
+ST, PEEE = ST_reduction(ST, Yc, Tq)
+i = 0
