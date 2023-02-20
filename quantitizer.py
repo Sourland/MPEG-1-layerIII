22b3e910ad6e5c0e7f24b4c68ccea57006bbaaea
Dimitrios Sourlantzis <jem2010@windowslive.com>
2023-02-20 10:20:22 +0200

    finish quantizer

--- quantitizer.py
+++ quantitizer.py
@@ -3,7 +3,8 @@ from helpers import dct_frequencies
 from scipy.io import wavfile
 from codec import coder
 from dct import frameDCT
-from tonal_components import psyco, Dk_Sparse
+from tonal_components import psycho, Dk_Sparse
+import matplotlib.pyplot as plt
 
 
 def critical_bands(K):
@@ -18,7 +19,7 @@ def critical_bands(K):
     """
     # Define the upper limits of the critical bands.
     band_limits = np.array([100, 200, 300, 400, 510, 630,
-                            770, 920, 1080, 1480, 1720,
+                            770, 920, 1080, 1270, 1480, 1720,
                             2000, 2320, 2700, 3150, 3700,
                             4400, 5300, 6400, 7700, 9500,
                             12000, 15500])
@@ -53,7 +54,7 @@ def DCT_band_scale(c: np.ndarray):
     cb = critical_bands(c.shape[0])
 
     # Apply the scaling rule to the DCT coefficients.
-    scaling_rule = np.power(np.abs(c), 0.75)
+    scaling_rule = np.power(np.abs(c), 3 / 4)
 
     # Initialize arrays to hold the scaled DCT coefficients and the scale factors.
     c_tilde = np.zeros(c.shape)
@@ -61,7 +62,7 @@ def DCT_band_scale(c: np.ndarray):
 
     # Scale the DCT coefficients for each critical band.
     for i in range(np.max(cb)):
-        dct_coeffs_band = np.where(cb == i + 1)[0]
+        dct_coeffs_band = np.where(cb - 1 == i)[0]
         max_scale_factor = np.max(scaling_rule[dct_coeffs_band])
         sign = np.sign(c[dct_coeffs_band])
         c_tilde[dct_coeffs_band] = np.multiply(sign, scaling_rule[dct_coeffs_band]) / max_scale_factor
@@ -72,37 +73,44 @@ def DCT_band_scale(c: np.ndarray):
 
 
 def quantizer(x: np.ndarray, b):
-    if np.abs(x) == 1 and b == 1:
+    if np.any(np.abs(x) == 1) and b == 1:
         return 0
 
-    w_b = 1 / (2 ** b - 1)
+    # Calculate the width of each quantizer zone
+    w_b = 1 / (2 ** b)
     zone_deciders = np.array([])
 
-    for i in range(- (2 ** b - 1), 2 ** b, 2):
-        zone_deciders = np.append(zone_deciders, i * w_b)
+    # Calculate the quantizer zones
+    for i in range(-2 ** (b - 1), 2 ** (b - 1) + 1):
+        zone_deciders = np.append(zone_deciders, 2 * i * w_b)
 
-    zone_deciders = zone_deciders[1:-1]
+    # Delete the 0 value element from the zones to make it dead-zone
+    zone_deciders = np.delete(zone_deciders[1:-1], np.where(zone_deciders[1:-1] == 0)[0])
+    positions = np.zeros(x.shape[0])
+    # Append the values to the zone boundaries and calculate their position
+    for i in range(x.shape[0]):
+        array = np.append(zone_deciders, x[i])
+        positions[i] = array.argsort().argsort()[-1]
 
-    array = np.append(zone_deciders, x)
-    position = array.argsort().argsort()[-1]
-    quantized_x = position - b
-
-    return quantized_x
+    return positions.astype(int)
 
 
 def dequantizer(synb_index, b):
-    w_b = 1 / (2 ** b - 1)
+    w_b = 1 / (2 ** b)
     zone_deciders = np.array([])
-    for i in range(- (2 ** b - 1), 2 ** b, 2):
-        zone_deciders = np.append(zone_deciders, i * w_b)
 
-    if b > 1:
-        return (zone_deciders[synb_index + b + 1] + zone_deciders[synb_index + b]) / 2
-    else:
-        return sum(zone_deciders) / 2
+    for i in range(-2 ** (b - 1), 2 ** (b - 1) + 1):
+        zone_deciders = np.append(zone_deciders, 2 * i * w_b)
+
+    zone_deciders = np.delete(zone_deciders, np.where(zone_deciders[1:-1] == 0)[0])
+
+    if b == 1:
+        return (sum(zone_deciders[1:-1]) / 2) * np.ones(np.array([synb_index]).shape)
+
+    return (zone_deciders[synb_index] + zone_deciders[synb_index + 1]) / 2
 
 
-def all_bands_quantizer(c: np.ndarray, Tg: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+def all_bands_quantizer(c: np.ndarray, Tg: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Applies a quantization algorithm to a vector of DCT coefficients to find the optimal amounds of bits required for
     quantization using a set of threshold values defined by the psychoacoustic model.
@@ -117,54 +125,99 @@ def all_bands_quantizer(c: np.ndarray, Tg: np.ndarray) -> Tuple[np.ndarray, np.n
             2. An array of scale factors for each band, used for rescaling coefficients.
             3. An array containing the optimal number of bits to use to quantize each coefficient
     """
+
+    cb = critical_bands(c.shape[0])
+
     # Scales the DCT coefficients to be in the range [-1, 1]
     c_tilde, scale_factors = DCT_band_scale(c)
 
     # Initializes the number of bits for quantization for each coefficient starting from 1
-    bits_per_coeff = np.ones(c.shape).astype(int)
+    bits_per_band = np.ones(np.unique(cb).shape).astype(int)
 
     # Initializes an array to store the quantized symbols for each coefficient
     symbols_c = np.zeros(c.shape)
 
     # Loops over each band in c_tilde
-    for i in range(c_tilde.shape[0]):
-        # Continuously quantizes the coefficients in the band until the power of the error does not exceed the psychoacoustic threshold
+    for i in np.unique(cb - 1):
+        # Take the indexes for every critical band
+        band_indexes = np.where(cb == i + 1)[0]
+
+        # Chooses components, thresholds and scaling factors of current band
+        band_components = c_tilde[band_indexes]
+        band_thresholds = Tg[band_indexes]
+        c_band = c[band_indexes]
+        sfs = scale_factors[band_indexes]
+
+        # Deletes elements where threshold is not a number (nan)
+        band_components_reduced = np.delete(band_components, np.isnan(band_thresholds))
+        c_band = np.delete(c_band, np.isnan(band_thresholds))
+        sfs = np.delete(sfs, np.isnan(band_thresholds))
+        band_thresholds = np.delete(band_thresholds, np.isnan(band_thresholds))
+
         while 1:
             # Quantizes the coefficients in the band using the current number of bits
-            c_symbol = quantizer(c_tilde[i], bits_per_coeff[i])
+            c_symbols = quantizer(band_components_reduced, bits_per_band[i])
 
-            # Dequantizes the coefficients using the same number of bits to get an approximation of the original coefficients
-            c_tilde_hat = dequantizer(c_symbol, bits_per_coeff[i])
+            # Dequantizes the coefficients using the same number of bits to get an approximation of the original
+            # coefficients
+            c_tilde_hat = dequantizer(c_symbols, bits_per_band[i])
 
             # Rescales the dequantized coefficients to their original values
-            c_hat = np.sign(c_tilde_hat) * c_tilde_hat * np.power(scale_factors[i], 4 / 3)
+            c_hat = np.sign(c_tilde_hat) * np.power(np.abs(c_tilde_hat) * sfs, 4 / 3)
 
             # Calculates the error between the original coefficients and the rescaled dequantized coefficients
-            e = np.abs(c_hat - c[i])
+            e = np.abs(c_hat - c_band)
 
             # Calculates the power of the error
             Pb = 10 * np.log10(e ** 2)
 
             # If the power of the error is greater than the psychoacoustic threshold, increment the number of bits
-            if Pb > Tg[i]:
-                bits_per_coeff[i] += 1
+            if np.any(Pb > band_thresholds):
+                bits_per_band[i] += 1
             else:
-            # Quantize using the optimal amount of bits and save
-                symbols_c[i] = quantizer(c_tilde[i], bits_per_coeff[i])
+                symbols_c[band_indexes] = quantizer(band_components, bits_per_band[i])
                 break
-
-    # Returns the optimally quantized symbols of the DCT coefficients, the scale factors, and the number of bits per coefficient
-    return symbols_c, scale_factors, bits_per_coeff
+    # Returns the optimally quantized symbols of the DCT coefficients, the scale factors, and the number of bits per
+    # coefficient
+    return symbols_c.astype(int), scale_factors, bits_per_band
 
 
 def all_bands_dequantizer(symb_index: np.ndarray, B: np.ndarray, SF: np.ndarray) -> np.ndarray:
-    c_dequantized = np.zeros(symb_index.shape)
+    """
+    Dequantizes symbols across all critical bands.
 
-    for i in range(symb_index.shape[0]):
-        c_dequantized[i] = dequantizer(symb_index[i], B[i])
+    Args:
+        symb_index (np.ndarray): 1-D array of quantized symbols.
+        B (np.ndarray): 1-D array of bits used to quantize every critical band
+        SF (np.ndarray): 1-D array of scaling factors for each critical band.
 
-    return np.sign(c_dequantized) * c_dequantized * np.power(SF, 4 / 3)
+    Returns:
+        np.ndarray: 1-D array of dequantized symbols.
+    """
+    # Determine the critical bands for each symbol
+    cb = critical_bands(symb_index.shape[0])
+    # Initialize array for dequantized symbols
+    dequantized_c = np.zeros(symb_index.shape)
 
+    # Dequantize symbols in each critical band
+    for i in np.unique(cb - 1):
+        # Take the indexes for every critical band
+        band_indexes = np.where(cb == i + 1)[0]
+
+        # Select symbols and scaling factors for the current critical band
+        band_symbols, sfs = symb_index[band_indexes], SF[band_indexes]
+
+        # Dequantize symbols using the band corresponding number of bits
+        c_tilde_hat = dequantizer(band_symbols, B[i])
+
+        # Apply inverse power scaling to the dequantized symbols
+        c_hat = np.sign(c_tilde_hat) * np.power(np.abs(c_tilde_hat) * sfs, 4 / 3)
+
+        # Store dequantized symbols in the corresponding indices
+        dequantized_c[band_indexes] = c_hat
+
+    # Return the array of dequantized symbols
+    return dequantized_c
 
 
 h = np.load("h.npy", allow_pickle=True).tolist()["h"]
@@ -173,14 +226,23 @@ L = 512
 samplerate, wavin = wavfile.read("myfile.wav")
 
 Y_tot = coder(wavin, h, M, N)
-Yc = Y_tot[:N, :]
+pepe = 0
+Yc = Y_tot[pepe * N:(pepe + 1) * N, :]
 Y_dct = frameDCT(Yc)
 
 c_hat, scales = DCT_band_scale(Y_dct)
-# x_quant = quantizer(c_hat, 2)
-# x = dequantizer(x_quant, 3)
+# for b in range(1, 17):
+#     x = np.random.rand(5)
+#     print(f"initial: {x}, b = {b}")
+#     x_quant = quantizer(x, b)
+#     # print(f"Symbols = {x_quant} for b = {b}\n")
+#     x = dequantizer(x_quant, b)
+#     print(f"Qd for b = {b}: {x}\n")
+
 D = Dk_Sparse(M * N - 1)
-Tg = psyco(Y_dct, D)
-symbols_c, scale_factors, bits_per_coeff = all_bands_quantizer(Y_dct, Tg)
-Yc_hat = all_bands_dequantizer(symbols_c, bits_per_coeff, scale_factors)
-omgomg = 0
+Tg = psycho(Y_dct, D)
+# plt.plot(Tg)
+# plt.show()
+symbols, sf, bits = all_bands_quantizer(Y_dct, Tg)
+Y_dct_hat = all_bands_dequantizer(symbols, bits, sf)
+haha = 0
